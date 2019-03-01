a2118d5e182e96f9a4843e026e78ca21c2f43e25
Milena Crivella <milenaverissimo@id.uff.br>
2019-03-01 15:02:43 -0300

    Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,18 +45,17 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_record.dump_schema_after_migration = false
+
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => "smtp.gmail.com",
-  :domain         => ENV['domain'],
-  :user_name      => ENV['gmail_username'],
-  :password       => ENV["gmail_password"],
-  :authentication => :plain,
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['gmail_username'],
+  :password             => ENV['gmail_password'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
 }
 
   # Raises error for missing translations
